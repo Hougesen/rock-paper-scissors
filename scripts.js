de64aa7c2b44da9a4b01151e772d11ad1b45f54b
Mads Hougesen <madshougesen@gmail.com>
2020-06-09 23:15:58 +0200

    rock paper scissors with gui

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-let playerScore = 0
-let computerScore = 0
+let playerScore = document.querySelector(".playerScore")
+let computerScore = document.querySelector(".computerScore")
+let tieScore = document.querySelector(".tieScore")
 
 
+//Function that randomly pick "rock", "paper" or "scissors" (the computer)
 function computerPlay() {
     let computerPlayPick = Math.floor(Math.random() * 3)
 
@@ -17,34 +19,47 @@ function computerPlay() {
 
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Pick one: Rock, paper or scissors").toLowerCase()
-    computerSelection = computerPlay()
+computerSelection = computerPlay()
 
-    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        playerScore++
-        console.log(`You win - ${playerSelection} beats ${computerSelection}. `)
-        return `You win - ${playerSelection} beats ${computerSelection}. `
-    } else if ((computerSelection == "rock" && playerSelection == "scissors") || (computerSelection == "paper" && playerSelection == "rock") || (computerSelection == "scissors" && playerSelection == "paper")) {
-        computerScore++
-        console.log(`You lose - ${computerSelection} beats ${playerSelection}. `)
-        return `You lose - ${computerSelection} beats ${playerSelection}. `
+
+//Function that activates when a button is clicked
+function buttonGame(playerSelection, computerSelection) {
+
+    if (playerSelection == 1) {
+        playerSelection = "rock"
+    } else if (playerSelection == 2) {
+        playerSelection = "paper"
     } else {
-        console.log(`Tie - You both picked ${playerSelection}. `)
-        return `Tie - You both picked ${playerSelection}. `
+        playerSelection = "scissors"
     }
 
 
-}
+    computerSelection = computerPlay()
 
+    let history = document.createElement("LI");
+    let historyWin = document.createTextNode(`You win - ${playerSelection} beats ${computerSelection}. `)
+    let historyLose = document.createTextNode(`You lose - ${computerSelection} beats ${playerSelection}. `)
+    let historyTie = document.createTextNode(`Tie - You both picked ${playerSelection}. `)
 
-function game(playerSelection, computerSelection) {
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
-    playRound(playerSelection, computerSelection)
-    console.log("Player Score: " + playerScore + ", Computer Score: " + computerScore);
+    if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
+        playerScore.innerHTML++
+        history.appendChild(historyWin);
+        document.getElementById("history").appendChild(history);
+        console.log(`You win - ${playerSelection} beats ${computerSelection}. `)
+        return `You win - ${playerSelection} beats ${computerSelection}. `
 
+    } else if ((computerSelection == "rock" && playerSelection == "scissors") || (computerSelection == "paper" && playerSelection == "rock") || (computerSelection == "scissors" && playerSelection == "paper")) {
+        computerScore.innerHTML++
+        history.appendChild(historyLose);
+        document.getElementById("history").appendChild(history);
+        console.log(`You lose - ${computerSelection} beats ${playerSelection}. `)
+        return `You lose - ${computerSelection} beats ${playerSelection}. `
 
+    } else {
+        tieScore.innerHTML++
+        history.appendChild(historyTie);
+        document.getElementById("history").appendChild(history);
+        console.log(`Tie - You both picked ${playerSelection}. `)
+        return `Tie - You both picked ${playerSelection}. `
+    }
 }
